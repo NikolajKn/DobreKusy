59a6ac64477df78311b3ddcf0cd25c436363d8a1
drab12 <47824573+drab12@users.noreply.github.com>
2020-11-30 11:09:33 +0100

    Add files via upload

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,25 +2,32 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { Nav } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-bootstrap';
 import './st.css'
 
-const Navbar = () => {
+const Navba = () => {
 
   return (
-    <Nav justify variant="tabs" defaultActiveKey="/home" bg-dark>
-  <Nav.Item>
-    <Nav.Link href="home">Home</Nav.Link>
+   
+ 
+  <Nav justify variant="tabs" defaultActiveKey="/home" className = "moja">
+      <Navbar className = "moja">
+  <Nav.Item >
+    <Nav.Link href="home"  className ="storagemaster">Stora<span className = "logo">g</span>e Master</Nav.Link>
   </Nav.Item>
   <Nav.Item>
-    <Nav.Link href="Menu">Menu</Nav.Link>
+    <Nav.Link href="Menu" className ="moj">Menu</Nav.Link>
   </Nav.Item>
   <Nav.Item>
-    <Nav.Link href="Recipes">Recipes</Nav.Link>
+    <Nav.Link href="Recipes" className ="moj">Recipes</Nav.Link>
   </Nav.Item>
   <Nav.Item>
-    <Nav.Link eventKey="Storage" href="Storage">Storage </Nav.Link>
+    <Nav.Link ev href="Storage" className ="moj">Storage </Nav.Link>
   </Nav.Item>
+  </Navbar>
 </Nav>
+
   )
 }
 
@@ -31,4 +38,4 @@ const Navbar = () => {
                 }
             }
 
-            export default connect(mapStateToProps)(Navbar)
+            export default connect(mapStateToProps)(Navba)
