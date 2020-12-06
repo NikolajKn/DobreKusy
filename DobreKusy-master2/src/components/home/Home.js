ed932eb7f16931372102b63965f8ff65ce307922
drab12 <47824573+drab12@users.noreply.github.com>
2020-12-06 18:28:01 +0100

    Homepage final

--- DobreKusy-master2/src/components/home/Home.js
+++ DobreKusy-master2/src/components/home/Home.js
@@ -7,14 +7,15 @@ import logo from './logo.png';
 import firebase from "firebase"
 import { useState } from "react";
 import HomeItem from "./HomeItem"
-import Menu from "../menu/MenuDetail";
+import Menu from "./MenuItem";
 import './styles.css'
 
 var mili = Date.now() +(86400*7*1000)
 var currentDate = new Date(mili) 
-var dat = Date.now()
-var endofMenuDate = Date.now() 
+var dat = (Date.now())/1000
+var endofMenuDate = (Date.now() - (86400*7*1000))/1000 
 var datum = new Date(Date.now()).toLocaleDateString();
+var ind = -5
 
 const Home = (props) => {
     var {storage,men} = props
@@ -22,34 +23,22 @@ const Home = (props) => {
     console.log(men)
     console.log(storage)
     
-    {
+   /* {
         men===undefined ?
         console.log("nenajdeny"): console.log(men[0].id)
-    }
+    }*/
 
    
         
     const datumCheck = () =>{
-        const pom = new Date(dat);
-        const day = pom.getDay()
-        if(day == 2){
-            endofMenuDate= endofMenuDate- (86400*2*1000)
+        for(let i=0; i<men.length;i++){
+           
+            if(men[i].date.seconds > endofMenuDate && men[i].date.seconds < dat ){
+                ind = i;
+                break;
+            }
         }
-        if(day == 3){
-            endofMenuDate= endofMenuDate- (86400*2*1000)
-        }
-        if(day == 4){
-            endofMenuDate= endofMenuDate- (86400*2*1000)
-        }
-        if(day == 5){
-            endofMenuDate= endofMenuDate- (86400*2*1000)
-        }
-        if(day == 6){
-            endofMenuDate= endofMenuDate- (86400*6*1000)
-        }
-        if(day == 7){
-            endofMenuDate= endofMenuDate- (86400*7*1000)
-        }
+       
 
     }
 
@@ -78,9 +67,9 @@ const Home = (props) => {
                 :
                
                <Container>
-               <h1 className="Nadpis1"> Vitajte v Storage Masteri <br/> {datum} </h1>
+               <h1 className="Nadpis1"> Welcome in Storage Master <br/> {datum} </h1>
                 <Row >            
-                    <Col > 
+                    <Col sm={5} > 
                      
                     
                                           <h1 style={{"color":"#069697"}} className="Nadpis">Expiration Ingredients</h1>
@@ -106,10 +95,18 @@ const Home = (props) => {
                         </tbody>   
                             </Table>                
                     </Col> 
-                     <Col >
 
-                                <Menu index={men[0].id} />    </Col>         </Row>  
-                                    </Container>
+                     <Col sm={10} >
+                           {datumCheck()}
+                           {ind ==-5 ?
+                         <div>    <h1> You dont have any menu on this week</h1> </div>
+                                :
+                                 <Menu index={men[ind].id} /> }
+                     </Col>       
+               </Row>  
+                                
+           </Container>
+                                
                                 
                            
                                   
@@ -136,5 +133,5 @@ const mapDispatchToProps = (dispatch) => {
 //{collection:"menu",orderBy:["date.seconds","asc"],where:['date.seconds','<=',dat]}
 export default compose(
     connect(mapStateToProps,mapDispatchToProps),
-    firestoreConnect([{collection:"storage",orderBy:["expirationDate","asc"],where:['expirationDate','<=',currentDate]},{collection:"menu",orderBy:["date.seconds","asc"],where:['date.seconds','<=',dat]}])
+    firestoreConnect([{collection:"storage",orderBy:["expirationDate","asc"],where:['expirationDate','<=',currentDate]},{collection:"menu",orderBy:["date.seconds","asc"],where:['date.seconds','>=',endofMenuDate]}])
 )(Home)
